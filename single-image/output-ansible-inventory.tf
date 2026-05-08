f7617d7596c1cd6ea27f0e4752cbf6fa022012ad
Devin Bayly <baylyd@email.arizona.edu>
2026-05-08 18:27:45 +0000

    testing out shelving and unshelving at the end

--- single-image/output-ansible-inventory.tf
+++ single-image/output-ansible-inventory.tf
@@ -19,8 +19,26 @@ resource "null_resource" "ansible-execution" {
         command = "ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_SSH_PIPELINING=True ANSIBLE_CONFIG=ansible.cfg ansible-playbook -i hosts.yml --forks=10 playbook.yml"
         working_dir = "${path.module}/ansible"
     }
+    provisioner "local-exec" {
+        command  = "pip3 install python-openstackclient"
+
+    }
 
     depends_on = [
         local_file.ansible-inventory,
     ]
+}
+
+resource "null_resource" "shelve-unshelve" {
+      count = var.instance_count
+
+      provisioner "local-exec" {
+        command = "openstack server shelve ${openstack_compute_instance_v2.os_instances[count.index].id} --wait"
+      }
+      provisioner "local-exec" {
+        command = "openstack server unshelve ${openstack_compute_instance_v2.os_instances[count.index].id} --wait"
+      }
+      depends_on = [
+        null_resource.ansible-execution
+      ]
 }
